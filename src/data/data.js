3833c4379a867fee1c75937aa694884b480ca058
Bibek_Ghoshal <bibekghoshal785@gmail.com>
2023-04-07 22:09:21 +0530

    First Commit

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+import Dosa from '../images/dosa.jpg';
+import Chola from '../images/chhola.jpg';
+import Idli from '../images/idli.jpg';
+import MasalaDosa from '../images/masala.jpg';
+import Paneer from '../images/paneer.jpg';
+import Gujrati from '../images/gujrati.jpeg';
+
+export const MenuList=[
+    {   id:1,
+        name:'Dosa',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:Dosa,
+        price:200
+    },
+    {    id:2,
+        name:'Chola',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:Chola,
+        price:120
+    },
+    {   id:3,
+        name:'Idli',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:Idli,
+        price:220
+    },
+    {   id:4,
+        name:'MasalaDosa',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:MasalaDosa,
+        price:300
+    },
+    {   id:5,
+        name:'Paneer',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:Paneer,
+        price:250
+    },
+    {   id:6,
+        name:'Gujrati',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, deserunt!',
+        image:Gujrati,
+        price:160
+    }
+];
+
